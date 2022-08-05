07f7c485b58c4c2fde7a606907a48811082be3d2
edmondywen <edmondywen@gmail.com>
2022-08-04 19:23:25 -0700

    fixed import errors (pls add to readme matt) and copied over previous progress (so much of it I swear)

---
+++ components/gallery.tsx
@@ -0,0 +1,10 @@
+import styles from '../styles/Home.module.css';
+import type { NextPage } from 'next';
+
+const Gallery: NextPage = () => {
+    return (
+        <p>Hello</p>
+    )
+}
+
+export default Gallery;
